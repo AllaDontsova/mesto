9792b59bc9aef831842f506c6ffbca9dba4f499c
Alla DOntsova <daa.ekb96@gmail.com>
2022-05-04 19:04:34 +0200

    After_1Review_Adaptiv

--- scripts/script.js
+++ scripts/script.js
@@ -7,28 +7,36 @@ const profileName = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const form = document.querySelector('.popup__container');
 
-popupOpenButton.addEventListener('click', function () {
-    completeForm();
-    togglePopup();
-});
-
-popupCloseButton.addEventListener('click', function () {
-    togglePopup();
-});
-
 function togglePopup() {
-    popup.classList.toggle('popup__opened');
-};
+    popup.classList.toggle('popup_opened');
+    if (popup.classList.contains('popup_opened')){
+        completeForm ();
+    } 
+}
 
 function completeForm() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileSubtitle.textContent;
 }
 
-form.addEventListener('submit', formSubmitHandler);
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
     togglePopup();
-};
+}
+
+function onPopupOpenButtonClick() {
+    togglePopup();
+}
+
+function onPopupCloseButtonClick() {
+    togglePopup();
+}
+
+popupOpenButton.addEventListener('click', onPopupOpenButtonClick);
+
+popupCloseButton.addEventListener('click', onPopupCloseButtonClick);
+
+form.addEventListener('submit', formSubmitHandler);
+
