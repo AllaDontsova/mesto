1f92c2bac307ac5e38c205129559b2804cdfc37c
Alla DOntsova <daa.ekb96@gmail.com>
2022-05-31 15:08:40 +0200

    Clear-AddCard_Function-escape

--- scripts/script.js
+++ scripts/script.js
@@ -17,24 +17,28 @@ const templateCard = document.querySelector('#cards');
 const cardsContainer = document.querySelector('.elements');
 const buttonClosePopupImage = document.querySelector('#closeImage');
 const popups = document.querySelectorAll('.popup');
+const image = document.querySelector('.popup__image');
+const title = document.querySelector('.popup__title');
 
 // Функции открытия и закрытия попапов
 function openPopup(popup) {
     popup.classList.add('popup_opened');
+    document.addEventListener('keydown', closePopupByEscape);
+    setEventListenerInput(popup, config);
 }
 
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
+    inputNameCardPopupAddCard.value = '';
+    inputLinkPopupAddCard.value = '';
+    document.removeEventListener('keydown', closePopupByEscape);
 }
 
 // Функции для открытия попап-профиль
 function openPopupProfile() {
-    openPopup(popupProfile);
     inputNamePopupProfile.value = profileName.textContent;
     inputPersonalInfoPopupProfile.value = profileSubtitle.textContent;
-    const inputList = Array.from(formProfileEdit.querySelector('.popup__input'));
-    const buttonSavePopup = formProfileEdit.querySelector('.popup__save');
-    offPopupSaveBtn(inputList, buttonSavePopup, config);
+    openPopup(popupProfile);
 }
 
 // Функции для закрытия попап-профиль
@@ -119,9 +123,7 @@ function createCard(nameCardValue, linkCardValue) {
     // Функция открытия попап-картинки
     function openPopupImage() {
         openPopup(popupImage);
-        const image = document.querySelector('.popup__image')
         image.src = linkCardValue;
-        const title = document.querySelector('.popup__title');
         title.textContent = nameCardValue;
         image.alt = `Фото ${nameCardValue}.`;
     }
@@ -132,20 +134,19 @@ function createCard(nameCardValue, linkCardValue) {
 // Функция закрытие попапа кнопкой Esc
 function closePopupByEscape(evt) {
     if (evt.key === 'Escape') {
-        const popupOpened = document.querySelector('.popup_opened');
-        closePopup(popupOpened);
+        const openedPopup = document.querySelector('.popup_opened');
+        closePopup(openedPopup);
     }
 };
 
-document.addEventListener('keydown', closePopupByEscape);
-
-// Функция закрытие попапа кликом на оверлей
+// Функция закрытие попапа кликом на оверлей и крестик
 popups.forEach((popup) => {
-    popup.addEventListener('click', closePopupByOverlay);
-});
-
-function closePopupByOverlay(evt) {
-    if (event.target == event.currentTarget) {
-        closePopup(evt.target);
-    };
-};
+    popup.addEventListener('mousedown', (evt) => {
+        if (evt.target.classList.contains('popup_opened')) {
+            closePopup(popup)
+        }
+        if (evt.target.classList.contains('popup__close')) {
+            closePopup(popup)
+        }
+    })
+})
